46e38668a62b33140d8c8e80f8e257fc1614772e
Mariano-MP <montielpenamariano@gmail.com>
2025-09-11 20:24:34 -0600

    Se realiza una corrutina para el boton de color

--- app/src/main/java/com/example/corrutinas/ui/main/CoroutineApp.kt
+++ app/src/main/java/com/example/corrutinas/ui/main/CoroutineApp.kt
@@ -1,5 +1,6 @@
 package com.example.corrutinas.ui.main
 
+import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.corrutinas.ui.theme.CorrutinasTheme
@@ -45,9 +47,9 @@ fun CoroutineApp(mainViewModel: MainViewModel, modifier : Modifier = Modifier){
         Text(text = mainViewModel.resultState)
         Spacer(modifier = modifier.height(30.dp))
         Button(onClick = {
-            mainViewModel.bloqueApp()
+            mainViewModel.fetchData()
         }) {
-            Text("Realizar Consulta")
+            Text(text = "Realiza Consulta")
         }
 
     }
--- app/src/main/java/com/example/corrutinas/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/corrutinas/viewmodel/MainViewModel.kt
@@ -4,13 +4,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
     var resultState by mutableStateOf("")
     private set
 
-    fun bloqueApp(){
-        Thread.sleep(5000)
-        resultState = "Resultado de la web"
+    fun fetchData(){
+        viewModelScope.launch {
+            delay(5000)
+            resultState = "Respuesta Obtenida por la Web"
+        }
     }
 }
