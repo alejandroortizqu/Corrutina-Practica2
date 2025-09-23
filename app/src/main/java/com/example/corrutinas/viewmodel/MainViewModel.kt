c6002321e75c02087f44df483f111745318b7644
Mariano-MP <montielpenamariano@gmail.com>
2025-09-23 00:05:45 -0600

    Version Final

--- app/src/main/java/com/example/corrutinas/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/corrutinas/viewmodel/MainViewModel.kt
@@ -5,37 +5,42 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
     var resultState by mutableStateOf("")
     var countTime by mutableStateOf(0)
-    private set
+        private set
     var oneCount by mutableStateOf(false)
 
+    private var contadorJob: Job? = null
 
-    fun contadorDoble(){
-        resultState = ""
-        oneCount = true
-        viewModelScope.launch {
-            for(i in 1..5){
-                delay(1000)
-                countTime = i
-            }
-            countTime = 0
-            for(i in 1..5){
-                delay(1000)
-                countTime = i
-            }
-            oneCount = false
-        }
-        viewModelScope.launch {
-            delay(10000)
-            resultState = "Fin de Contadores"
+    private suspend fun contadorSimple(tiempo: Int) {
+        for (i in 1..tiempo) {
+            delay(1000)
+            countTime = i
         }
     }
+
+    fun contadorDoble() {
+        resultState = ""
+        oneCount = true
+        countTime = 0
+
+        contadorJob = viewModelScope.launch {
+            contadorSimple(5)
+
+            countTime = 0
+            contadorSimple(5)
+
+            resultState = "Fin de Contadores"
+            oneCount = false
+        }
+    }
+
     fun cancelarContador() {
         viewModelScope.coroutineContext.cancelChildren()
         countTime = 0
